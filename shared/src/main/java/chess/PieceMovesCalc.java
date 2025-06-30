a567332a5a30bc5fcf156f53dbfc941fbbd2622b
Alissa Moulder <allenali@byu.edu>
2025-06-30 15:46:13 -0600

    queen

--- shared/src/main/java/chess/PieceMovesCalc.java
+++ shared/src/main/java/chess/PieceMovesCalc.java
@@ -9,8 +9,8 @@ public class PieceMovesCalc {
         switch (piece.getPieceType()) {
             case KING:
                 return calcKingMoves(board, position, piece);
-            // case QUEEN:
-            //    return calcQueenMoves(board, position, piece);
+            case QUEEN:
+                return calcQueenMoves(board, position, piece);
             case BISHOP:
                 return calcBishopMoves(board, position, piece);
             //case ROOK:
@@ -53,15 +53,45 @@ public class PieceMovesCalc {
         return moves;
     }
 
-    //  private Collection<ChessMove> calcQueenMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
+    private Collection<ChessMove> calcQueenMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
+        List<ChessMove> moves = new ArrayList<>();
+        int[][] direction = {
+                {0, 1}, {1, 0}, {0, -1}, {-1, 0},//left, right, up down
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1}//diagonals
+        };
+        for (int[] dir : direction) {
+            int newRow = position.getRow();
+            int newCol = position.getColumn();
+            while (true) {
+                newRow += dir[0];
+                newCol += dir[1];
+                if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) { // make sure it's in the bounds of the chess board
+                    //cant be less than one or greater than 8
+                    break;
+                }
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece target = board.getPiece(newPos);
+                if (target == null) {
+                    moves.add(new ChessMove(position, newPos, null));
+                } else {
+                    if (target.getTeamColor() != piece.getTeamColor()) {
+                        moves.add(new ChessMove(position, newPos, null));
+                    }
+                    break;
+                }
+            }
+        }
 
-    //}
+        return moves;
+    }
+
+
     private Collection<ChessMove> calcBishopMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
         List<ChessMove> moves = new ArrayList<>();
-        int[][] directions = {
+        int[][] direction = {
                 {1, 1}, {1, -1}, {-1, 1}, {-1, -1} //up right, up left, down right, down left
         };
-        for (int[] dir : directions) {
+        for (int[] dir : direction) {
             int newRow = position.getRow();
             int newCol = position.getColumn();
             while (true) {
@@ -87,6 +117,7 @@ public class PieceMovesCalc {
         return moves;
     }
 }
+
   //  private Collection<ChessMove> calcQueenMoves(ChessBoard board, ChessPosition position, ChessPiece piece) {
   //      List<ChessMove> moves = new ArrayList<>();
    //     int[][] directions = {
