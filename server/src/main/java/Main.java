a13ad9cbc650814e0b89979b3ca1ae23875b00d7
Alissa Moulder <allenali@byu.edu>
2025-07-16 21:31:50 -0600

    finished getting started page

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,11 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
+        var port = 8080;
+        var server = new Server();
+        server.run(port);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
