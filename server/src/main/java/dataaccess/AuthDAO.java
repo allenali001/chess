38c0e5332f8fd3144d190c7007bd4c751c068917
Alissa Moulder <allenali@byu.edu>
2025-07-17 19:00:19 -0600

    skeleton of project

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,15 @@
+package dataaccess;
+import models.AuthData;
+
+
+public class AuthDAO {
+
+}
+
+/*clear: A method for clearing all data from the database. This is used during testing.
+
+       createAuth: Create a new authorization.
+        getAuth: Retrieve an authorization given an authToken.
+deleteAuth: Delete an authorization so that it is no longer valid.
+
+ */
---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,13 @@
+package dataaccess;
+import models.UserData;
+
+
+
+public class UserDAO {
+}
+
+
+/*createUser: Create a new user.
+        getUser: Retrieve a user with the given username.
+
+ */
