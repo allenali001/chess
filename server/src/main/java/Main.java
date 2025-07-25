3b4d46f71f46ae5b7c1f842cfd8336f9f8684026
Alissa Moulder <allenali@byu.edu>
2025-07-25 16:40:33 -0600

    main

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,12 +1,4 @@
-import dataaccess.AuthDaoSql;
-import dataaccess.daos.AuthDAO;
-import dataaccess.daos.GameDAO;
-import dataaccess.daos.UserDAO;
-import dataaccess.GameDaoSql;
-import dataaccess.memoryClasses.AuthDaoMemory;
-import dataaccess.memoryClasses.GameDaoMemory;
-import dataaccess.memoryClasses.UserDaoMemory;
-import dataaccess.UserDaoSql;
+import dataaccess.*;
 import server.Server;
 import service.AuthService;
 import service.ClearService;
@@ -20,22 +12,21 @@ public class Main {
                 if (args.length >= 1) {
                     port = Integer.parseInt(args[0]);
                 }
-
-                UserDAO userDAO = new UserDaoMemory();
-                GameDAO gameDAO = new GameDaoMemory();
-                AuthDAO authDAO = new AuthDaoMemory();
-                if (args.length >= 2 && args[1].equals("sql")) {
-                    userDAO = new UserDaoSql();
-                    gameDAO = new GameDaoSql();
-                    authDAO= new AuthDaoSql();
+                DataAccess dataAccess = new MemoryDataAccess();
+                if (args.length >= 2 && args[1].equalsIgnoreCase("sql")) {
+                    dataAccess = new MySqlDataAccess();
                 }
 
+                UserDAO userDAO = dataAccess.getUserDAO();
+                GameDAO gameDAO = dataAccess.getGameDAO();
+                AuthDAO authDAO = dataAccess.getAuthDAO();
+
                 var authService = new AuthService(authDAO);
-                var gameService =new GameService(gameDAO, authService);
+                var clearService = new ClearService(userDAO, authDAO, gameDAO);
+                var gameService = new GameService(gameDAO, authService);
                 var userService = new UserService(userDAO, authDAO);
-                var clearService = new ClearService(userDAO,authDAO,gameDAO);
-                var server = new Server(userService,gameService, clearService);
-                port=server.run(port);
+
+                port = new Server(userService, gameService, clearService).run(port);
                 System.out.printf("Server started on port %d with UserDAO: %s, GameDAO: %s, AuthDAO: %s%n", port,
                         userDAO.getClass().getSimpleName(),
                         gameDAO.getClass().getSimpleName(),
